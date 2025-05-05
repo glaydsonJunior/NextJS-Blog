2445a4503e5ecbb0c4b1562a2ffc5bc3d2d15b16
Glaydson <glaydsoncomp15@gmail.com>
2025-05-05 15:17:44 +0100

    Pages Route Completed!

--- components/layout.js
+++ components/layout.js
@@ -4,10 +4,12 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
  
-const name = 'Your Name';
+
 export const siteTitle = 'Next.js Sample Website';
  
 export default function Layout({ children, home }) {
+  const name = 'Glaydson';
+
   return (
     <div className={styles.container}>
       <Head>
